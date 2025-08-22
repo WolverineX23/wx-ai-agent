5b3e4c8dd453648c4d002e84195a2445da77afe8
wx <547036318@qq.com>
2025-08-22 11:39:01 +0800

    add: DashScope SDK 接入测试

---
+++ src/main/java/com/wolf/wxaiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,10 @@
+package com.wolf.wxaiagent.demo.invoke;
+
+/**
+ * 仅用于测试获取 API_KEY； - 建议配置 API Key 到环境变量，避免将 API Key 明文写在代码中，从而降低泄漏风险
+ */
+public interface TestApiKey {
+
+    String API_KEY = "你的 DashScope-Api-Key";
+
+}
